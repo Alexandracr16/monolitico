7b0205eb69dbd7e3e4aefc083965409094acd995
DAVID ALEJANDRO BUITRAGO ORJUELA <buitrago2051@outlook.com>
2025-10-30 00:03:05 -0500

    Ajustes Pantalla Principal

--- views/principal.php
+++ views/principal.php
@@ -8,30 +8,15 @@
 
 </head>
 <body>
-<<<<<<< HEAD
-    <h1>
-        Bienvenido a la Aplicación que cuenta el amor 
-        que se tiene por la programación como el amor que se tiene 
-        David y jessy  una pareja hermosa, divina y perfecta. 
-    </h1>
-    <a href= "estudiante.php" >Estudiante</a><br>
-    <a href= "nota.php" >Notas</a><br>
-    <a href="/monolitico/controllers/materia-controller.php?action=listar">Materias</a><br>
-    <a href="/monolitico/controllers/programa-controller.php?action=listar">Programas</a>
-=======
 <div class="contenedor">
     <h1>Universidad Y. J. and D.</h1>
     <p>Sistema de gestión académica</p>
-    
     <div class="contenedor-enlaces">
         <a href="estudiante.php" class="enlace-navegacion"><span>Estudiante</span></a>
         <a href="nota.php" class="enlace-navegacion"><span>Notas</span></a>
-        <a href="/monolitico/controllers/materia-controller.php?action=listar">Materias</a><br>
+        <a href="/monolitico/controllers/materia-controller.php?action=listar" class="enlace-navegacion">Materias</a>
         <a href="/monolitico/controllers/programa-controller.php?action=listar" class="enlace-navegacion"><span>Programas</span></a>
     </div>
-    
 </div>
->>>>>>> f2687d0d310176fc117e453bbf1dd3172333884d
-
 </body>
 </html>
